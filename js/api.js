63320ddb192c08b875960bab850d0ab1730e4338
Foxxy1201 <93086871+Foxxy1201@users.noreply.github.com>
2026-04-13 03:26:34 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 // ============================================
 // CONFIG — GANTI INI
 // ============================================
-const API_BASE = 'https://gasoline-qty-pasta-mental.trycloudflare.com'; // ← ganti URL Cloudflare Tunnel lo
+const API_BASE = 'https://api.cashorbit.my.id'; // ← ganti URL Cloudflare Tunnel lo
 
 // ============================================
 // GLOBAL STATE
